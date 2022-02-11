7004260e9c32042cc43cd8e84b5d581f660aa78b
usman1100 <usmanahmed1100@gmail.com>
2022-02-11 23:35:19 +0500

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import {Response} from "express"
+
+@Controller()
+export class AppController {
+  constructor() {}
+
+  @Get()
+  getHello(@Res() res:Response) {
+    return res.json({
+      message: "Welcome to EffortAero REST API",
+      version: "1.0.0",
+    })
+  }
+}
