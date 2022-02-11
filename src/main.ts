e8ec7a0544e57d4bdbd5f83c91ff4b58149ba0c9
usman1100 <usmanahmed1100@gmail.com>
2022-02-11 23:52:59 +0500

    .env setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+
+const dotenv = require("dotenv")
+dotenv.config("../");
+
+
+
+async function bootstrap() {  
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(env.PORT ||  3000);
 }
 bootstrap();
