2466d684404df1b38c579fda82ed2640a364d2a4
usman1100 <usmanahmed1100@gmail.com>
2022-02-20 18:01:12 +0500

    Refactoring Removed create route from users Renamed all route to search

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,6 @@
-import {
-    Body,
-    Controller,
-    Get,
-    Post,
-    Req,
-    Res,
-    UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { UserDTO } from './schemas/user.dto';
 import { UserService } from './user.service';
 
 @Controller('users')
@@ -17,16 +8,9 @@ import { UserService } from './user.service';
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
-    @Get('all')
-    async getAll(@Req() req: any, @Res() res: Response) {
+    @Get('search')
+    async getAll(@Res() res: Response) {
         const response = await this.userService.getAll();
-        return res.status(response.code).json(response);
-    }
-
-    @Post('create')
-    async create(@Body() req: UserDTO, @Res() res: Response) {
-        const response = await this.userService.create(req);
-
         return res.status(response.code).json(response);
     }
 }
